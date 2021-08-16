ef8ca8df1fc05b3b62a316fd764ec1c940d7b257
dennieljoshua <denniel.joshua@outlook.com>
2021-08-17 02:34:26 +0800

    Enable sync even in production :poop:

--- src/db/init.ts
+++ src/db/init.ts
@@ -3,7 +3,7 @@ import Todo from './models/Todo';
 const isDev = process.env.NODE_ENV === 'development';
 
 const dbInit = () => {
-	Todo.sync({ alter: isDev });
+	Todo.sync({ alter: true });
 };
 
 export default dbInit;
