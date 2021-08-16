7021dbaa36e217663ba17e2857d08e03d43702ea
dennieljoshua <denniel.joshua@outlook.com>
2021-08-17 04:12:15 +0800

    Fix self signed ssl issue

--- src/db/config.ts
+++ src/db/config.ts
@@ -22,7 +22,10 @@ if (process.env.NODE_ENV === 'production') {
 		dialect: 'postgres',
 		protocol: 'postgres',
 		dialectOptions: {
-			ssl: true,
+			ssl: {
+				require: true,
+				rejectUnauthorized: false,
+			},
 		},
 	});
 } else {
