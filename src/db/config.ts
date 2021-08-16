ef9ae035695290c118ec77a6b0ae278b34ce0d11
dennieljoshua <denniel.joshua@outlook.com>
2021-08-17 02:33:21 +0800

    Use heroku database on production

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,12 +1,20 @@
 import { Sequelize } from 'sequelize';
 
 // Setup database
-const sequelize = new Sequelize({
-	dialect: 'postgres',
-	username: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	database: process.env.DB_NAME,
-	host: process.env.DB_HOST,
-});
+let sequelize: Sequelize;
+if (process.env.NODE_ENV === 'production') {
+	if (!process.env.DATABASE_URL) {
+		throw new Error('Missing Heroku Postgress Environment Variable');
+	}
+	sequelize = new Sequelize(process.env.DATABASE_URL);
+} else {
+	sequelize = new Sequelize({
+		dialect: 'postgres',
+		username: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		database: process.env.DB_NAME,
+		host: process.env.DB_HOST,
+	});
+}
 
 export default sequelize;
